8e37a57649a25e4b2e0148fb1d9edc1aab61639c
itingtseng <42088639+itingtseng@users.noreply.github.com>
2024-02-21 18:53:28 -0500

    05-reverse.js solved

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -12,11 +12,26 @@ reverse("id"); // "di"
 reverse(""); // ""
 ***********************************************************************/
 
+//base: str.length == 0
+//recursive: str.length > 0; str.length<length
+//step: move from end to start
 
 function reverse(str) {
   // Your code here 
+  if (str.length == 0) {
+    return str
+  }
+  else {
+    return str.slice(-1) + reverse(str.slice(0, -1))
+  }
 }
 
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
