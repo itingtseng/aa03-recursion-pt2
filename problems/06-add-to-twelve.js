1be57b601fe73607eae8e96f9ada49135807b732
itingtseng <42088639+itingtseng@users.noreply.github.com>
2024-02-21 19:06:56 -0500

    06-add-to-twelve.js

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -12,11 +12,46 @@ addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
 
+//base: arr.length == 0
+//recursive: 
+//step: 
 
 function addToTwelve(arr) {
   // Your code here 
+  if (arr.length == 0) {
+    return false
+  }
+  else if (arr[0]+ arr[1] == 12) {
+    return true
+  }
+  else {
+    arr.shift()
+    return addToTwelve(arr)
+  }
 }
 
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+console.log(addToTwelve([1, 11, 4, 7, 6])); // true
+console.log(addToTwelve([1, 12, 4, 7, 6])); // false
+console.log(addToTwelve([1])); // false
+
+// base flavors = 0
+// recursive flavors.pop() !== favorite
+// step flavors.pop
+
+function iceCreamShop(flavors, favorite) {
+
+  if (flavors.length == 0) {
+    return false
+  }
+  else if (flavors.pop() == favorite) {
+    return true
+  }
+  else {
+    return (iceCreamShop(flavors, favorite))
+  }
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
